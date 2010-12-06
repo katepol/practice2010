e7520ddc48b32425aaf972da8a0c2c73e7c4c008
katepol14 <katepol14@293393a8-d42e-468b-6850-6a0227143aea>
2010-12-06 02:22:21 +0000

    delete redundant files from NaiveBayesianClassifier

--- NaiveBayesianClassifier/Checkers.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MyCheckers
-#define _MyCheckers
-
-void testIsCorrect (FILE* f, int shift, char const * str, std::string const & msg);
-void Check (FILE* f, std::string const & ext);
-
-#endif
